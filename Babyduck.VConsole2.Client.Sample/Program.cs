9cc1081d35a218dbf164e49155da864f6609e6e9
tintin <tintin@babyduck.tw>
2026-02-23 16:58:58 +0800

    Initial Commit

---
+++ Babyduck.VConsole2.Client.Sample/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Babyduck.VConsole2.Client.Sample;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Babyduck.VConsole2.Client/Class1.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace Babyduck.VConsole2.Client
+{
+    public class Class1 { }
+}
